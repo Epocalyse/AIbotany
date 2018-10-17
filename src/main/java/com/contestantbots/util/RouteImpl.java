6a47e9c946bf7900dc098b1a9ac6e5fbf4ace063
norab7 <sb100@hw.ac.uk>
2018-10-17 16:14:35 +0100

    Fixed RouteImpl

--- src/main/java/com/contestantbots/util/RouteImpl.java
+++ src/main/java/com/contestantbots/util/RouteImpl.java
@@ -1,25 +1,34 @@
 package com.contestantbots.util;
 
+import com.scottlogic.hackathon.game.Player;
+import com.scottlogic.hackathon.game.Position;
+import com.scottlogic.hackathon.game.Route;
+
 public class RouteImpl implements Comparable<Route> {
-    private final Player player;
-    private final Position destination;
-    private final int distance;
-    public RouteImpl(Player player, Position destination, int distance) {
-        this.player = player;
-        this.destination = destination;
-        this.distance = distance;
-    }
-    public Player getPlayer() {
-        return player;
-    }
-    public Position getDestination() {
-        return destination;
-    }
-    public int getDistance() {
-        return distance;
-    }
-    @Override
-    public int compareTo(Route o) {
-        return distance - o.getDistance();
-    }
+	private final Player player;
+	private final Position destination;
+	private final int distance;
+
+	public RouteImpl(Player player, Position destination, int distance) {
+		this.player = player;
+		this.destination = destination;
+		this.distance = distance;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public Position getDestination() {
+		return destination;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	@Override
+	public int compareTo(Route o) {
+		return distance - ((RouteImpl) o).getDistance();
+	}
 }
