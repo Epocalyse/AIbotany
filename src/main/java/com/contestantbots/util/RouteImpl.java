b4d6582ac0acc15c3b65a9953c520398a1af8a18
Thispered <hcohen123@gmail.com>
2018-10-17 16:33:48 +0100

    Random methods in Route. Yay

--- src/main/java/com/contestantbots/util/RouteImpl.java
+++ src/main/java/com/contestantbots/util/RouteImpl.java
@@ -31,4 +31,31 @@ public class RouteImpl implements Comparable<Route> {
 	public int compareTo(Route o) {
 		return distance - ((RouteImpl) o).getDistance();
 	}
+
+	private List<Route> generateRoutes(final GameState gameState, Set<Player> players, Set<Position> destinations) {
+		List<Route> routes = new ArrayList<>();
+		for (Position destination : destinations) {
+			for (Player player : players) {
+				int distance = gameState.getMap().distance(player.getPosition(), destination);
+				Route route = new Route(player, destination, distance);
+				routes.add(route);
+			}
+		}
+		return routes;
+	}
+
+	private List<Move> assignRoutes(final GameState gameState, final Map<Player, Position> assignedPlayerDestinations, final List<Position> nextPositions, List<Route> routes) {
+		return routes.stream()
+				.filter(route -> !assignedPlayerDestinations.containsKey(route.getPlayer())&& !assignedPlayerDestinations.containsValue(route.getDestination()))
+				.map(route -> {
+					Optional<Direction> direction = gameState.getMap().directionsTowards(route.getPlayer().getPosition(), route.getDestination()).findFirst();
+					if (direction.isPresent() && canMove(gameState, nextPositions, route.getPlayer(), direction.get())) {
+						assignedPlayerDestinations.put(route.getPlayer(), route.getDestination());
+						return new MoveImpl(route.getPlayer().getId(), direction.get());
+					}
+					return null;
+				})
+				.filter(move -> move != null)
+				.collect(Collectors.toList());
+	}
 }
